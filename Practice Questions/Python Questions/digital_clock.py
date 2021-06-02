3da54e8e0c3abc35589f62508615208f1821ace1
Tanzeel Khan <ktanzeel80@gmail.com>
2021-06-02 20:46:00 +0530

    Learned how to make a digital clock

--- Practice Questions/Python Questions/digital_clock.py
+++ Practice Questions/Python Questions/digital_clock.py
@@ -1,11 +1,25 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import font
 import time
+import datetime
 
-while True:
-    t = time.localtime()
-    print("\n")
-    print("|--------------------------|")
-    print("|--------------------------|")
-    print("|{}hours {}minutes {}seconds.|".format(t.tm_hour, t.tm_min, t.tm_sec))
-    print("|--------------------------|")
-    print("|--------------------------|")
-    time.sleep(1)
+def quit(*args):
+    root.destroy() 
+
+def clock():
+    time = datetime.datetime.now()
+    time = (time.strftime("%H:%M:%S"))
+    txt.set(time)
+    root.after(1000, clock)
+
+root = Tk()
+root.attributes("-fullscreen", False)
+root.configure(background="pink")
+root.bind("x", quit)
+root.after(1000, clock)
+fnt = font.Font(family="Helvetica", size=120, weight="bold")
+txt = StringVar()
+lbl = ttk.Label(root, textvariable=txt, font=fnt, foreground="black", background="pink")
+lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+root.mainloop()
