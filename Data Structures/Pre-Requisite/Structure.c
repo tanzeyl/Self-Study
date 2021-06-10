a721997643e7163f300250ae3a240eda53d4d958
Tanzeel Khan <ktanzeel80@gmail.com>
2021-06-10 13:12:31 +0530

    Learning DSA.

--- Data Structures/Pre-Requisite/Structure.c
+++ Data Structures/Pre-Requisite/Structure.c
@@ -11,7 +11,7 @@ int main()
     // Declaring and initializing a structure
     struct Rectangle r = {10,5}; 
     // This will declare a variable r of the type Rectangle and it will take 8 bytes of memory.
-    // Since we declared length first, the first 2 bytes will occupy lenght and rest 2 will occupy breadth. 
+    // Since we declared length first, the first 4 bytes will occupy lenght and rest 4 will occupy breadth. 
     //Line 12 will therefore assign 10 as length and 5 as breadth.
     //Accessing the members:
     r.length = 15;
