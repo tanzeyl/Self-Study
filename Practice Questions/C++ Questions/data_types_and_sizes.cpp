97fd945db8059ac184bcd82042b22bdee3bcc94f
Tanzeel Khan <ktanzeel80@gmail.com>
2021-07-03 16:03:56 +0530

    Added Ruby

---
+++ Practice Questions/C++ Questions/data_types_and_sizes.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout<<"Size of char is "<<sizeof(char)<<" bytes"<<endl;
+    cout<<"Size of int is "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"Size of short int is "<<sizeof(short int)<<" bytes"<<endl;
+}
---
+++ Ruby/todo.rb
@@ -0,0 +1,5 @@
+# To print todo list
+puts "What is your next todo?"
+next_todo = gets
+puts "\n\n\n"
+puts "[ ] #{next_todo}"
