7ab6d3eabb0a75e7da990635eee2f043cbb25ce6
Tanzeel Khan <ktanzeel80@gmail.com>
2021-07-25 13:41:47 +0530

    Learning ReactJS

--- React by Mosh Hamedani/counting-app/src/components/counter.jsx
+++ React by Mosh Hamedani/counting-app/src/components/counter.jsx
@@ -1,48 +1,25 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: [],
-  };
-
-  styles = {
-    fontWeight: 700,
-    fontSize: 50,
-  };
-
-  renderTags() {
-    if (this.state.tags.length === 0)
-      return <p>There are no tags to display.</p>;
-
-    return (
-      <ol>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ol>
-    );
-  }
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
   render() {
     return (
       <>
         <div>
-          <span style={this.styles} className={this.getBadgeClasses()}>
+          <span className={this.getBadgeClasses()}>
             {this.functionCounter()}
           </span>
-
           <button
-            onClick={this.handleIncrement}
+            onClick={() => this.props.onIncrement(this.props.counter)}
             className="btn btn-secondary btn-sm"
           >
             Increment
           </button>
-          {this.state.tags.length === 0 && "Please create some new tags!!"}
-          {this.renderTags()}
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm m-2"
+          >
+            Delete
+          </button>
         </div>
       </>
     );
@@ -50,13 +27,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-4 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   functionCounter() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
--- React by Mosh Hamedani/counting-app/src/index.js
+++ React by Mosh Hamedani/counting-app/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import App from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+import Counters from "./components/counters";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
