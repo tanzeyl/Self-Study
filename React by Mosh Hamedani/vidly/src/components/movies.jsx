ccc2ea48dff8337bb9c8e85b1b0e1ab81c15d24a
Tanzeel Khan <ktanzeel80@gmail.com>
2021-07-26 11:35:35 +0530

    Learning React

--- React by Mosh Hamedani/vidly/src/components/movies.jsx
+++ React by Mosh Hamedani/vidly/src/components/movies.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    heart: <i class="fa fa-heart-o" aria-hidden="true"></i>,
   };
 
   handleDelete = (movie) => {
@@ -14,6 +16,14 @@ class Movies extends Component {
   zeroHandler = () => {
     if (this.state.movies.length === 0) return <h1>There are no movies.</h1>;
     return <h1>There are {this.state.movies.length} movies in this</h1>;
+  };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
   };
 
   render() {
@@ -28,6 +38,7 @@ class Movies extends Component {
               <th>Stock</th>
               <th>Daily Rate</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -37,6 +48,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     className="btn-danger btn-sm"
