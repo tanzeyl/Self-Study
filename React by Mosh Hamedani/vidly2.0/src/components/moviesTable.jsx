e93989e57671f87abd1f6bc18d65fdd8515956a3
Tanzeel Khan <ktanzeel80@gmail.com>
2021-07-28 19:07:36 +0530

    Learning ReactJS

--- React by Mosh Hamedani/vidly2.0/src/components/moviesTable.jsx
+++ React by Mosh Hamedani/vidly2.0/src/components/moviesTable.jsx
@@ -1,19 +1,16 @@
 import React, { Component } from "react";
 import Like from "./common/like";
+import TableHeader from "./common/tableHeader";
 
 class MoviesTable extends Component {
-  raiseSort = (path) => {
-    const sortColumn = { ...this.props.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-    this.props.onSort(sortColumn);
-  };
+  columns = [
+    { path: "title", label: "Title" },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentRate", label: "Daily Rate" },
+  ];
   render() {
-    const { movies, onDelete, onLike, onSort } = this.props;
+    const { movies, onDelete, onLike } = this.props;
     return (
       <table className="table">
         <thead>
