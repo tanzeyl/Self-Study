aebd6afc4b44302ba5667b869901b7070faa57d4
Tanzeel Khan <ktanzeel80@gmail.com>
2021-08-19 19:46:32 +0530

    Learning DS

--- web-101/Module 8/main.js
+++ web-101/Module 8/main.js
@@ -1,21 +1,68 @@
 let c = document.getElementById("myCanvas");
 let ctx = c.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(100, 100, 5, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.moveTo(100, 105);
-ctx.lineTo(100, 120);
-ctx.stroke();
-ctx.moveTo(100, 120);
-ctx.lineTo(110, 140);
-ctx.stroke();
-ctx.moveTo(100, 120);
-ctx.lineTo(90, 140);
-ctx.stroke();
-ctx.moveTo(100, 110);
-ctx.lineTo(110, 115);
-ctx.stroke();
-ctx.moveTo(100, 110);
-ctx.lineTo(90, 115);
-ctx.stroke();
+let drawLine = (x, y) => {
+  ctx.lineTo(x, y);
+  ctx.stroke();
+};
+
+let drawCircle = (x, y, r, sAngle, eAngle, color) => {
+  ctx.beginPath();
+  ctx.arc(x, y, r, sAngle, eAngle);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.stroke();
+};
+
+//Head
+drawCircle(250, 250, 150, 0, 2 * Math.PI, "transparent");
+
+//Eyes
+let drawEyes = (radius, color) => {
+  drawCircle(300, 200, radius, 0, 2 * Math.PI, color);
+  drawCircle(200, 200, radius, 0, 2 * Math.PI, color);
+};
+
+//Nose
+let drawNose = (type) => {
+  if (type === "crooked") {
+    ctx.moveTo(250, 225);
+    drawLine(200, 275);
+    ctx.moveTo(200, 275);
+    drawLine(250, 275);
+  } else {
+    drawCircle(250, 250, 20, 0, Math.PI, "transparent");
+    ctx.moveTo(230, 250);
+    drawLine(250, 225);
+    ctx.moveTo(270, 250);
+    drawLine(250, 225);
+  }
+};
+
+//Mouth
+let drawMouth = (expression) => {
+  if (expression === "surprised") {
+    drawCircle(250, 325, 40, 0, 2 * Math.PI, "transparent");
+  } else if (expression === "happy") {
+    drawCircle(250, 300, 40, 0, Math.PI, "transparent");
+  } else {
+    ctx.moveTo(200, 325);
+    drawLine(300, 325);
+  }
+};
+
+let getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+let drawRandomFace = () => {
+  let eyeRadius = getRndInteger(1, 20);
+  let eyeColor = ["blue", "brown", "green"][getRndInteger(0, 3)];
+  let noseShape = ["crooked", "button"][getRndInteger(0, 2)];
+  let expression = ["surprised", "happy", "flat"][getRndInteger(0, 3)];
+  drawEyes(eyeRadius, eyeColor);
+  drawNose(noseShape);
+  drawMouth(expression);
+};
+
+drawRandomFace();
