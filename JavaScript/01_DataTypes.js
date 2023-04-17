c01f7ed91156c411d0e2ce7edccfd3493fca2ce1
Tanzeel <ktanzeel80@gmail.com>
2023-04-17 12:10:13 +0530

    Web D

---
+++ JavaScript/01_DataTypes.js
@@ -0,0 +1,17 @@
+a = null;
+b = 34;
+c = "Tanzeel";
+d = Symbol("My name is Tanzeel");
+e = true;
+f = BigInt(289990);
+g = undefined;
+
+console.log(a, b, c, d, e, f, g);
+console.log(typeof d);
+
+const traits = {
+  Name: "Tanzeel",
+  Age: 22,
+  College: "GEU",
+};
+console.log(traits["Name"]);
